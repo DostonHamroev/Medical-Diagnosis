a396e41c070f62c0c0fce66201b14b9a2b834318
doston <dos400dos400@gmail.com>
2021-12-03 21:55:35 +0500

    start project

---
+++ app/src/main/java/uz/hamroev/medicaldiagnosis/MainActivity.kt
@@ -0,0 +1,32 @@
+package uz.hamroev.medicaldiagnosis
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import uz.hamroev.medicaldiagnosis.activity.HomeActivity
+import uz.hamroev.medicaldiagnosis.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 2500)
+    }
+}
---
+++ app/src/main/java/uz/hamroev/medicaldiagnosis/activity/HomeActivity.kt
@@ -0,0 +1,58 @@
+package uz.hamroev.medicaldiagnosis.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import uz.hamroev.medicaldiagnosis.R
+import uz.hamroev.medicaldiagnosis.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setContentView(binding.root)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext, "Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_result -> Toast.makeText(applicationContext, "Result", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Settings", Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext, "Share", Toast.LENGTH_SHORT).show()
+                R.id.nav_rate -> Toast.makeText(applicationContext, "Rate us", Toast.LENGTH_SHORT).show()
+                R.id.nav_about -> Toast.makeText(applicationContext, "About App", Toast.LENGTH_SHORT).show()
+
+            }
+
+            true
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
