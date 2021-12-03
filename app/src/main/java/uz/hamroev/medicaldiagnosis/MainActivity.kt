44c0187ef581bfe1f206dc220ac58159ca093aa3
doston <dos400dos400@gmail.com>
2021-12-03 23:53:38 +0500

    start project 2

--- app/src/main/java/uz/hamroev/medicaldiagnosis/MainActivity.kt
+++ app/src/main/java/uz/hamroev/medicaldiagnosis/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
         supportActionBar?.hide()
 
         Handler(Looper.getMainLooper()).postDelayed({
--- app/src/main/java/uz/hamroev/medicaldiagnosis/activity/HomeActivity.kt
+++ app/src/main/java/uz/hamroev/medicaldiagnosis/activity/HomeActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import com.google.android.material.navigation.NavigationView
 import uz.hamroev.medicaldiagnosis.R
 import uz.hamroev.medicaldiagnosis.databinding.ActivityHomeBinding
@@ -25,18 +26,26 @@ class HomeActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
-        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //supportActionBar?.setHomeButtonEnabled(true)
+        // supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        supportActionBar?.title = "Diagnostic"
         navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.nav_home -> Toast.makeText(applicationContext, "Home", Toast.LENGTH_SHORT).show()
-                R.id.nav_result -> Toast.makeText(applicationContext, "Result", Toast.LENGTH_SHORT).show()
-                R.id.nav_settings -> Toast.makeText(applicationContext, "Settings", Toast.LENGTH_SHORT).show()
-                R.id.nav_share -> Toast.makeText(applicationContext, "Share", Toast.LENGTH_SHORT).show()
-                R.id.nav_rate -> Toast.makeText(applicationContext, "Rate us", Toast.LENGTH_SHORT).show()
-                R.id.nav_about -> Toast.makeText(applicationContext, "About App", Toast.LENGTH_SHORT).show()
+            when (it.itemId) {
+                R.id.nav_share -> Toast.makeText(applicationContext, "Share", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.nav_rate -> Toast.makeText(applicationContext, "Rate us", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.nav_about -> Toast.makeText(
+                    applicationContext,
+                    "About App",
+                    Toast.LENGTH_SHORT
+                ).show()
 
             }
 
@@ -47,12 +56,16 @@ class HomeActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-
+        if (toggle.onOptionsItemSelected(item)) {
 
             return true
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.my_nav_host_fragment_container).navigateUp()
     }
 }
